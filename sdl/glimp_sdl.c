9a0503a074dd7a58e183e17e86f267452e38723f
Peter Mackay <mackay.pete+git@gmail.com>
2010-09-26 11:12:43 +0100

    Started on SDL port.

---
+++ sdl/glimp_sdl.c
@@ -0,0 +1,71 @@
+/*
+Copyright (C) 1997-2001 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+#include "../ref_gl/gl_local.h"
+
+#include <SDL_video.h>
+
+void		GLimp_BeginFrame( float camera_separation )
+{
+}
+
+void		GLimp_EndFrame( void )
+{
+	SDL_GL_SwapBuffers();
+}
+
+qboolean	GLimp_Init( void *hinstance, void *hWnd )
+{
+	return true;
+}
+
+void		GLimp_Shutdown( void )
+{
+}
+
+int     	GLimp_SetMode( int *pwidth, int *pheight, int mode, qboolean fullscreen )
+{
+	int width;
+	int height;
+	SDL_Surface *screen;
+
+	if ( !ri.Vid_GetModeInfo( &width, &height, mode ) )
+	{
+		ri.Con_Printf( PRINT_ALL, " invalid mode\n" );
+		return rserr_invalid_mode;
+	}
+
+	screen = SDL_SetVideoMode(width, height, 0, SDL_OPENGL);
+	if (screen == NULL)
+	{
+		return rserr_invalid_mode;
+	}
+
+	*pwidth = screen->w;
+	*pheight = screen->h;
+	
+	// let the sound and input subsystems know about the new window
+	ri.Vid_NewWindow (width, height);
+
+	return rserr_ok;
+}
+
+void		GLimp_AppActivate( qboolean active )
+{
+}
---
+++ sdl/sys_sdl.c
@@ -0,0 +1,187 @@
+/*
+Copyright (C) 1997-2001 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+#include "../qcommon/qcommon.h"
+#include <SDL.h>
+
+void *GetGameAPI (void *import);
+
+int	curtime;
+unsigned	sys_frame_time;
+
+static byte	*membase;
+static int	hunkmaxsize;
+static int	cursize;
+
+void Sys_mkdir (char *path)
+{
+}
+
+void Sys_Error (char *error, ...)
+{
+	va_list		argptr;
+
+	printf ("Sys_Error: ");	
+	va_start (argptr,error);
+	vprintf (error,argptr);
+	va_end (argptr);
+	printf ("\n");
+
+	exit (1);
+}
+
+void Sys_Quit (void)
+{
+	exit (0);
+}
+
+void	Sys_UnloadGame (void)
+{
+}
+
+void	*Sys_GetGameAPI (void *parms)
+{
+	return GetGameAPI(parms);
+}
+
+char *Sys_ConsoleInput (void)
+{
+	return NULL;
+}
+
+void	Sys_ConsoleOutput (char *string)
+{
+}
+
+void Sys_SendKeyEvents (void)
+{
+}
+
+void Sys_AppActivate (void)
+{
+}
+
+void Sys_CopyProtect (void)
+{
+}
+
+char *Sys_GetClipboardData( void )
+{
+	return NULL;
+}
+
+void	*Hunk_Begin (int maxsize)
+{
+	// reserve a huge chunk of memory, but don't commit any yet
+	cursize = 0;
+	hunkmaxsize = maxsize;
+
+	membase = malloc (maxsize);
+	memset (membase, 0, maxsize);
+
+	if (!membase)
+		Sys_Error ("Hunk_Begin reserve failed");
+
+	return (void *)membase;
+}
+
+void	*Hunk_Alloc (int size)
+{
+	// round to cacheline
+	size = (size+31)&~31;
+
+	cursize += size;
+	if (cursize > hunkmaxsize)
+		Sys_Error ("Hunk_Alloc overflow");
+
+	return (void *)(membase+cursize-size);
+}
+
+void	Hunk_Free (void *buf)
+{
+	free (buf);
+}
+
+int		Hunk_End (void)
+{
+	return cursize;
+}
+
+int		Sys_Milliseconds (void)
+{
+	return SDL_GetTicks();
+}
+
+void	Sys_Mkdir (char *path)
+{
+}
+
+char	*Sys_FindFirst (char *path, unsigned musthave, unsigned canthave)
+{
+	return NULL;
+}
+
+char	*Sys_FindNext (unsigned musthave, unsigned canthave)
+{
+	return NULL;
+}
+
+void	Sys_FindClose (void)
+{
+}
+
+void	Sys_Init (void)
+{
+}
+
+
+//=============================================================================
+
+int main (int argc, char **argv)
+{
+	qboolean go = true;
+	SDL_Event e;
+
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK) >= 0)
+	{
+		Qcommon_Init (argc, argv);
+
+		while (go)
+		{
+			while (SDL_PollEvent(&e))
+			{
+				switch (e.type)
+				{
+				case SDL_QUIT:
+					go = false;
+					break;
+				}
+			}
+
+			Qcommon_Frame (0.1);
+		}
+
+		SDL_Quit();
+	}
+
+	return 0;
+}
+
+
