c83eeac8a8e40f5eb2a37f02cdcb163856b4154e
Peter Mackay <mackay.pete+git@gmail.com>
2012-05-03 20:57:19 +0100

    Cleaned up some hunk init handling. Stats on Hunk_Free.

--- qcommon/mem.c
+++ qcommon/mem.c
@@ -26,11 +26,15 @@ struct hunk_s
 	int cursize;
 	int capacity;
 	char *membase;
+	const char *name;
+	int tidemark;
 };
 
-#define GAME_HUNK_CAPACITY (16 * 1024 * 1024)
-#define LEVEL_HUNK_CAPACITY (16 * 1024 * 1024)
-#define REF_HUNK_CAPACITY (16 * 1024 * 1024)
+#define HUNK_ALIGNMENT 8
+
+#define GAME_HUNK_CAPACITY (1 * 1024 * 1024)
+#define LEVEL_HUNK_CAPACITY (32 * 1024)
+#define REF_HUNK_CAPACITY (7 * 1024 * 1024)
 
 static char game_mem[GAME_HUNK_CAPACITY];
 static char level_mem[LEVEL_HUNK_CAPACITY];
@@ -40,35 +44,55 @@ hunk_t hunk_game;
 hunk_t hunk_level;
 hunk_t hunk_ref;
 
+static void Hunk_Init (hunk_t *hunk, const char *name, int capacity, void *membase)
+{
+	const int base_as_int = (int)membase;
+
+	if ((base_as_int % HUNK_ALIGNMENT) != 0)
+	{
+		Sys_Error ("Hunk_Init: Hunk \"%s\" membase is not aligned.", name);
+	}
+
+	hunk->cursize = 0;
+	hunk->capacity = capacity;
+	hunk->membase = membase;
+	hunk->name = name;
+	hunk->tidemark = 0;
+}
+
 void Mem_Init (void)
 {
-	hunk_game.cursize = 0;
-	hunk_game.capacity = GAME_HUNK_CAPACITY;
-	hunk_game.membase = game_mem;
-
-	hunk_level.cursize = 0;
-	hunk_level.capacity = LEVEL_HUNK_CAPACITY;
-	hunk_level.membase = level_mem;
-
-	hunk_ref.cursize = 0;
-	hunk_ref.capacity = REF_HUNK_CAPACITY;
-	hunk_ref.membase = ref_mem;
+	Hunk_Init (&hunk_game, "Game", GAME_HUNK_CAPACITY, game_mem);
+	Hunk_Init (&hunk_level, "Level", LEVEL_HUNK_CAPACITY, level_mem);
+	Hunk_Init (&hunk_ref, "Refresh", REF_HUNK_CAPACITY, ref_mem);
 }
 
 void *Hunk_Alloc (hunk_t *hunk, int size)
 {
 	// round to size of double
-	size = (size+7)&~7;
+	size = (size+HUNK_ALIGNMENT-1)&~(HUNK_ALIGNMENT-1);
 
 	hunk->cursize += size;
 	if (hunk->cursize > hunk->capacity)
 		Sys_Error ("Mem_Alloc overflow");
+
+	if (hunk->cursize > hunk->tidemark)
+	{
+		hunk->tidemark = hunk->cursize;
+	}
 
 	return (hunk->membase+hunk->cursize-size);
 }
 
 void Hunk_Free (hunk_t *hunk)
 {
+	Com_Printf(
+		"Hunk_Free: Freeing hunk \"%s\". Current size = %d, tidemark = %d (%d%% of capacity).\n",
+		hunk->name,
+		hunk->cursize,
+		hunk->tidemark,
+		(hunk->tidemark * 100) / hunk->capacity);
+
 	if (hunk->cursize > 0)
 	{
 		memset(hunk->membase, 0, hunk->cursize);
