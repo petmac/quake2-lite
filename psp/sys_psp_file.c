f758dea65e355b5501222d49ae94822aa60f74c1
Peter Mackay <mackay.pete+git@gmail.com>
2014-09-15 01:22:29 +0100

    File cache to mitigate cost of small reads.

--- psp/sys_psp_file.c
+++ psp/sys_psp_file.c
@@ -21,6 +21,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../qcommon/qcommon.h"
 #include <pspiofilemgr.h>
 
+#define CACHE_SIZE 1024
+
+static SceUID cache_file = -1;
+static size_t cache_file_pos;
+static size_t cache_begin;
+static size_t cache_end;
+static uint8_t cache[CACHE_SIZE];
+
 static file_t *quake_file(SceUID file)
 {
 	if (file < 0)
@@ -45,27 +53,113 @@ static SceUID native_file(file_t *file)
 	}
 }
 
+static size_t read_from_cache(uint8_t *dst, size_t bytes)
+{
+	const size_t cache_used = cache_end - cache_begin;
+	if (cache_used > 0)
+	{
+		const size_t use_from_cache = (bytes < cache_used) ? bytes : cache_used;
+
+		memcpy(dst, &cache[cache_begin], use_from_cache);
+		
+		cache_begin += use_from_cache;
+		cache_file_pos += use_from_cache;
+
+		return use_from_cache;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+static void fill_cache()
+{
+	cache_begin = 0;
+	cache_end = sceIoRead(cache_file, &cache[cache_begin], CACHE_SIZE);
+}
+
 file_t *Sys_OpenFileRead(const char *path)
 {
-	SceUID f = sceIoOpen(path, PSP_O_RDONLY, 0777);
+	const SceUID f = sceIoOpen(path, PSP_O_RDONLY, 0777);
 	return quake_file(f);
 }
 
 void Sys_CloseFile(file_t *file)
 {
-	SceUID f = native_file(file);
+	const SceUID f = native_file(file);
+
+	if (f == cache_file)
+	{
+		cache_file = -1;
+	}
+
 	sceIoClose(f);
 }
 
 size_t Sys_ReadFile(void *buffer, size_t size, size_t count, file_t *file)
 {
-	SceUID f = native_file(file);
-	return sceIoRead(f, buffer, size * count) / size;
+	const SceUID f = native_file(file);
+
+	uint8_t *dst = buffer;
+	size_t bytes_left = size * count;
+	size_t bytes_read = 0;
+
+	if (f == cache_file)
+	{
+		// Read as much as we need from the cache
+		bytes_read = read_from_cache(dst, bytes_left);
+		bytes_left -= bytes_read;
+
+		// Finished?
+		if (bytes_left == 0)
+		{
+			return bytes_read / size;
+		}
+
+		dst += bytes_read;
+	}
+	else
+	{
+		if (cache_file >= 0)
+		{
+			sceIoLseek32(cache_file, cache_file_pos, PSP_SEEK_SET);
+		}
+
+		cache_file = f;
+		cache_file_pos = sceIoLseek32(f, 0, PSP_SEEK_CUR);
+		cache_begin = 0;
+		cache_end = 0;
+	}
+
+	// The cache is now empty.
+	// Can the rest fit in the cache?
+	if (bytes_left <= CACHE_SIZE)
+	{
+		// Use the cache.
+		fill_cache();
+		bytes_read += read_from_cache(dst, bytes_left);
+	}
+	else
+	{
+		// Skip the cache.
+		size_t bytes_read_from_file = sceIoRead(f, dst, bytes_left);
+		bytes_read += bytes_read_from_file;
+		cache_file_pos += bytes_read_from_file;
+	}
+
+	return bytes_read / size;
 }
 
 long Sys_SeekFile(file_t *file, long offset, int whence)
 {
-	SceUID f = native_file(file);
+	const SceUID f = native_file(file);
+
+	if (f == cache_file)
+	{
+		cache_file = -1;
+		sceIoLseek32(f, cache_file_pos, PSP_SEEK_SET);
+	}
 
 	int wh = 0;
 	switch (whence)
@@ -86,6 +180,14 @@ long Sys_SeekFile(file_t *file, long offset, int whence)
 
 long Sys_TellFile(file_t *file)
 {
-	SceUID f = native_file(file);
-	return sceIoLseek32(f, 0, PSP_SEEK_CUR);
+	const SceUID f = native_file(file);
+
+	if (f == cache_file)
+	{
+		return cache_file_pos;
+	}
+	else
+	{
+		return sceIoLseek32(f, 0, PSP_SEEK_CUR);
+	}
 }
