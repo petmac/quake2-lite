ca3117d221dc479025c7593758525913cc440a43
Peter Mackay <mackay.pete+git@gmail.com>
2014-09-15 02:35:16 +0100

    Add enough cached files so all files required to load the demos are cached.

--- psp/sys_psp_file.c
+++ psp/sys_psp_file.c
@@ -21,13 +21,19 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../qcommon/qcommon.h"
 #include <pspiofilemgr.h>
 
+#define CACHE_COUNT 7
 #define CACHE_SIZE 1024
 
-static SceUID cache_file = -1;
-static size_t cache_file_pos;
-static size_t cache_begin;
-static size_t cache_end;
-static uint8_t cache[CACHE_SIZE];
+typedef struct cache_s
+{
+	SceUID file;
+	size_t file_pos;
+	size_t begin;
+	size_t end;
+	uint8_t buffer[CACHE_SIZE];
+} cache_t;
+
+static cache_t caches[CACHE_COUNT];
 
 static file_t *quake_file(SceUID file)
 {
@@ -53,17 +59,34 @@ static SceUID native_file(file_t *file)
 	}
 }
 
-static size_t read_from_cache(uint8_t *dst, size_t bytes)
+static cache_t *cache_find(SceUID f)
 {
-	const size_t cache_used = cache_end - cache_begin;
+	int i;
+
+	for (i = 0; i < CACHE_COUNT; ++i)
+	{
+		cache_t *const cache = &caches[i];
+
+		if (cache->file == f)
+		{
+			return cache;
+		}
+	}
+
+	return NULL;
+}
+
+static size_t read_from_cache(cache_t *cache, uint8_t *dst, size_t bytes)
+{
+	const size_t cache_used = cache->end - cache->begin;
 	if (cache_used > 0)
 	{
 		const size_t use_from_cache = (bytes < cache_used) ? bytes : cache_used;
 
-		memcpy(dst, &cache[cache_begin], use_from_cache);
+		memcpy(dst, &cache->buffer[cache->begin], use_from_cache);
 		
-		cache_begin += use_from_cache;
-		cache_file_pos += use_from_cache;
+		cache->begin += use_from_cache;
+		cache->file_pos += use_from_cache;
 
 		return use_from_cache;
 	}
@@ -73,25 +96,43 @@ static size_t read_from_cache(uint8_t *dst, size_t bytes)
 	}
 }
 
-static void fill_cache()
+static void fill_cache(cache_t *cache)
 {
-	cache_begin = 0;
-	cache_end = sceIoRead(cache_file, &cache[cache_begin], CACHE_SIZE);
+	cache->begin = 0;
+	cache->end = sceIoRead(cache->file, &cache->buffer[cache->begin], CACHE_SIZE);
 }
 
 file_t *Sys_OpenFileRead(const char *path)
 {
+	static qboolean cache_initialised = false;
+
 	const SceUID f = sceIoOpen(path, PSP_O_RDONLY, 0777);
+
+	if (!cache_initialised)
+	{
+		int i;
+
+		cache_initialised = true;
+
+		for (i = 0; i < CACHE_COUNT; ++i)
+		{
+			cache_t *const cache = &caches[i];
+
+			cache->file = -1;
+		}
+	}
+
 	return quake_file(f);
 }
 
 void Sys_CloseFile(file_t *file)
 {
 	const SceUID f = native_file(file);
+	cache_t *const cache = cache_find(f);
 
-	if (f == cache_file)
+	if (cache != NULL)
 	{
-		cache_file = -1;
+		cache->file = -1;
 	}
 
 	sceIoClose(f);
@@ -100,15 +141,16 @@ void Sys_CloseFile(file_t *file)
 size_t Sys_ReadFile(void *buffer, size_t size, size_t count, file_t *file)
 {
 	const SceUID f = native_file(file);
+	cache_t *cache = cache_find(f);
 
 	uint8_t *dst = buffer;
 	size_t bytes_left = size * count;
 	size_t bytes_read = 0;
 
-	if (f == cache_file)
+	if (cache != NULL)
 	{
 		// Read as much as we need from the cache
-		bytes_read = read_from_cache(dst, bytes_left);
+		bytes_read = read_from_cache(cache, dst, bytes_left);
 		bytes_left -= bytes_read;
 
 		// Finished?
@@ -121,15 +163,17 @@ size_t Sys_ReadFile(void *buffer, size_t size, size_t count, file_t *file)
 	}
 	else
 	{
-		if ((cache_file >= 0) && (cache_begin != cache_end))
+		cache = cache_find(-1);
+
+		if (cache == NULL)
 		{
-			sceIoLseek32(cache_file, cache_file_pos, PSP_SEEK_SET);
+			return sceIoRead(f, buffer, bytes_left) / size;
 		}
 
-		cache_file = f;
-		cache_file_pos = sceIoLseek32(f, 0, PSP_SEEK_CUR);
-		cache_begin = 0;
-		cache_end = 0;
+		cache->file = f;
+		cache->file_pos = 0;
+		cache->begin = 0;
+		cache->end = 0;
 	}
 
 	// The cache is now empty.
@@ -137,15 +181,15 @@ size_t Sys_ReadFile(void *buffer, size_t size, size_t count, file_t *file)
 	if (bytes_left <= CACHE_SIZE)
 	{
 		// Use the cache.
-		fill_cache();
-		bytes_read += read_from_cache(dst, bytes_left);
+		fill_cache(cache);
+		bytes_read += read_from_cache(cache, dst, bytes_left);
 	}
 	else
 	{
 		// Skip the cache.
 		size_t bytes_read_from_file = sceIoRead(f, dst, bytes_left);
 		bytes_read += bytes_read_from_file;
-		cache_file_pos += bytes_read_from_file;
+		cache->file_pos += bytes_read_from_file;
 	}
 
 	return bytes_read / size;
@@ -154,40 +198,41 @@ size_t Sys_ReadFile(void *buffer, size_t size, size_t count, file_t *file)
 long Sys_SeekFile(file_t *file, long offset, int whence)
 {
 	const SceUID f = native_file(file);
+	cache_t *const cache = cache_find(f);
 
-	if (f == cache_file)
+	if (cache != NULL)
 	{
-		const size_t buffer_file_pos_begin = cache_file_pos - cache_begin;
-		const size_t buffer_file_pos_end = buffer_file_pos_begin + cache_end;
+		const size_t buffer_file_pos_begin = cache->file_pos - cache->begin;
+		const size_t buffer_file_pos_end = buffer_file_pos_begin + cache->end;
 
 		switch (whence)
 		{
 		case SEEK_SET:
 			if ((offset >= buffer_file_pos_begin) && (offset <= buffer_file_pos_end))
 			{
-				cache_file_pos = offset;
-				cache_begin = offset - buffer_file_pos_begin;
+				cache->file_pos = offset;
+				cache->begin = offset - buffer_file_pos_begin;
 			}
 			else
 			{
-				cache_file_pos = sceIoLseek32(f, offset, PSP_SEEK_SET);
-				cache_begin = 0;
-				cache_end = 0;
+				cache->file_pos = sceIoLseek32(f, offset, PSP_SEEK_SET);
+				cache->begin = 0;
+				cache->end = 0;
 			}
 			break;
 		case SEEK_CUR:
-			cache_file_pos = sceIoLseek32(f, cache_file_pos + offset, PSP_SEEK_SET);
-			cache_begin = 0;
-			cache_end = 0;
+			cache->file_pos = sceIoLseek32(f, cache->file_pos + offset, PSP_SEEK_SET);
+			cache->begin = 0;
+			cache->end = 0;
 			break;
 		case SEEK_END:
-			cache_file_pos = sceIoLseek32(f, offset, PSP_SEEK_END);
-			cache_begin = 0;
-			cache_end = 0;
+			cache->file_pos = sceIoLseek32(f, offset, PSP_SEEK_END);
+			cache->begin = 0;
+			cache->end = 0;
 			break;
 		}
 
-		return cache_file_pos;
+		return cache->file_pos;
 	}
 	else
 	{
@@ -212,10 +257,11 @@ long Sys_SeekFile(file_t *file, long offset, int whence)
 long Sys_TellFile(file_t *file)
 {
 	const SceUID f = native_file(file);
+	cache_t *const cache = cache_find(f);
 
-	if (f == cache_file)
+	if (cache != NULL)
 	{
-		return cache_file_pos;
+		return cache->file_pos;
 	}
 	else
 	{
